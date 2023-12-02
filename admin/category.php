83d734cd7576a830acd7f3dfc1b0fbb6dc11706c
hvsonn <hvson2003@gmail.com>
2023-12-02 22:26:11 +0700

    update category

--- admin/category.php
+++ admin/category.php
@@ -1,30 +1,26 @@
 <?php
-include("model/connect.php");
-
+include("./model/category.php");
 if(isset($_GET["action"])){
 
     if($_GET["action"] == "add"){
         $tentheloai = (isset($_POST["tentheloai"])) ? $_POST["tentheloai"] : "";
-
-        $sql = "INSERT INTO theloai (ten_tloai) VALUES ('" . $tentheloai ."')";
-        $connect->query($sql);
+        $category = new Category("", $tentheloai);
+        $category->themtheloai();
         header("Location: ../admin/view/category.php");
     }
 
     if($_GET["action"] == "edit"){
         $matheloai = (isset($_POST["matheloai"])) ? $_POST["matheloai"] : "";
         $tentheloai = (isset($_POST["tentheloai"])) ? $_POST["tentheloai"] : "";
-
-        $sql = "UPDATE theloai SET ten_tloai='" . $tentheloai . "' WHERE ma_tloai=" . $matheloai;
-        $connect->query($sql);
+        $category = new Category($matheloai, $tentheloai);
+        $category->suatheloai($matheloai);
         header("Location: ../admin/view/category.php");
     }
 
     if($_GET["action"] == "delete"){
         $id = (isset($_GET["id"])) ? $_GET["id"] : "";
-
-        $sql = "DELETE FROM theloai WHERE ma_tloai=" . $id;
-        $connect->query($sql);
+        $category = new Category($id,"");
+        $category->xoatheloai($id);
         header("Location: ../admin/view/category.php");
     }
 }
